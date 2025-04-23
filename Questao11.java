a7c7a43ae0cae8047fbe8939830c8c0d2d75ab33
M-msdias <ddiasmarcela@gmail.com>
2025-04-23 11:01:12 -0300

    Create Questao11.java

---
+++ Questao11.java
@@ -0,0 +1,13 @@
+public class Questao11 {
+    public static long fatorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * fatorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fatorial(5)); 
+    }
+}
